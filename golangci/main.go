0fb48a179ff921bf562dccaf447ffa7ceb82ed13
d-kuro <kurosawa7620@gmail.com>
2018-10-01 20:00:36 +0900

    Add main.go

--- golangci/main.go
+++ golangci/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+// 使用しない定数
+const Url = "http://example.com"
+
+type Person struct {
+	name string
+	// 使用されない
+	age int
+}
 
 func main() {
-	fmt.Println("todo")
+	unconvert := "unconvert"
+	// 意味のないキャスト
+	unconvert = string(unconvert)
+	
+	person := &Person{name:"hoge"}
+	fmt.Println(person.name)
+	
+	// エラーチェックしない
+	returnError()
+}
+
+func returnError() error {
+	fmt.Println("return error")
+	return errors.New("new error")
+}
+
+// 使われない関数
+func unused() {
+	fmt.Println("unused")
 }
