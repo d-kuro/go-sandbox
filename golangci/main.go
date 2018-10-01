1d763a04602d9dd9bbd136c159a3dd72284ffe8b
d-kuro <kurosawa7620@gmail.com>
2018-10-02 00:41:10 +0900

    comment out.

--- golangci/main.go
+++ golangci/main.go
@@ -6,24 +6,26 @@ import (
 )
 
 // 使用しない定数
-const Url = "http://example.com"
+//const Url = "http://example.com"
 
 type Person struct {
 	name string
 	// 使用されない
-	age int
+	//age int
 }
 
 func main() {
-	unconvert := "unconvert"
+	//unconvert := "unconvert"
 	// 意味のないキャスト
-	unconvert = string(unconvert)
+	//unconvert = string(unconvert)
 	
-	person := &Person{name:"hoge"}
-	fmt.Println(person.name)
+	//person := &Person{name:"hoge"}
+	//fmt.Println(person.name)
 	
 	// エラーチェックしない
-	returnError()
+	if err := returnError(); err != nil {
+		fmt.Println("error")
+	}
 }
 
 func returnError() error {
@@ -32,6 +34,6 @@ func returnError() error {
 }
 
 // 使われない関数
-func unused() {
-	fmt.Println("unused")
-}
+//func unused() {
+//	fmt.Println("unused")
+//}
