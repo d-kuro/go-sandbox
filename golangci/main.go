a067cc855813e1a743e5f29e2b9e3b3e13c1272d
d-kuro <kurosawa7620@gmail.com>
2018-10-02 01:14:02 +0900

    インデントおかしい

--- golangci/main.go
+++ golangci/main.go
@@ -1,39 +1,7 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
-
-// 使用しない定数
-//const Url = "http://example.com"
-
-//type Person struct {
-	//name string
-	// 使用されない
-	//age int
-//}
+import "fmt"
 
 func main() {
-	//unconvert := "unconvert"
-	// 意味のないキャスト
-	//unconvert = string(unconvert)
-	
-	//person := &Person{name:"hoge"}
-	//fmt.Println(person.name)
-	
-	// エラーチェックしない
-	if err := returnError(); err != nil {
-		fmt.Println("error")
-	}
+fmt.Println("hoge")
 }
-
-func returnError() error {
-	fmt.Println("return error")
-	return errors.New("new error")
-}
-
-// 使われない関数
-//func unused() {
-//	fmt.Println("unused")
-//}
