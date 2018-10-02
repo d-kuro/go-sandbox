a075c21b594b3e5af25ce5f1de1bbfebb04b76b4
d-kuro <kurosawa7620@gmail.com>
2018-10-02 10:46:24 +0900

    hoge

--- golangci/main.go
+++ golangci/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+// 使用しない定数
+const Url = "http://example.com"
+
+type Person struct {
+	name string
+	// 使用されない
+	age int
+}
 
 func main() {
-fmt.Println("hoge")
+	// 意味のない代入
+	var ineffassign = "hoge"
+	ineffassign  = "ineffassign"
+	
+	fmt.Println(ineffassign)
+	
+	person := &Person{name:"hoge"}
+	fmt.Println(person.name)
+	
+	// エラーチェックしない
+	returnError()
+}
+
+func returnError() error {
+	fmt.Println("return error")
+	return errors.New("new error")
+}
+
+// 使われない関数
+func unused() {
+	fmt.Println("unused")
 }
